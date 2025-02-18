511956c2d90da75b24602524d0757d6d38a6e5c4
Deepak Papney <deepak.papney@abacusdesk.in>
2025-02-18 12:49:02 +0530

    changes made for datatypes

--- Math_functions/Basics/datatypes.js
+++ Math_functions/Basics/datatypes.js
@@ -1,2 +1,47 @@
 "use strict"; // to use newer versions of js .
 // alert(3+3);  not allowed in node js
+
+let name = "Manish Singh";
+let weight = 85;
+let isLogin = false;
+
+
+// string enclosed within single quotes
+let fruit = 'apple';
+console.log(fruit)
+
+// string enclosed within double quotes
+let country = "USA";
+console.log(country);
+
+// string enclosed within backticks
+let result = `fail`;
+console.log(result);
+
+// integer value
+let integer_number = -3;
+console.log(integer_number);
+
+// floating-point value
+let float_number = 3.15;
+console.log(float_number);
+
+
+
+/*
+PREMITIVE DATATYPES:
+->number
+->bigInt
+->boolean
+->string=" "
+->null=standalone Value(Denotes a null value.) let a = null;
+->undefined= A data type whose variable is not initialized.(let a;)
+->symbol= to find uniqueness
+
+OBECTS:
+Key-value pairs of collection of data.	let student = {name: "John"};
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+*/
